df955c63688fcbcf813eff51ffa15bcc22b4bd29
Ryan Johnson <ryan@tenthirtyam.org>
2021-11-02 12:37:48 -0400

    Update example
    Update the examples example

--- vsphere/vsphere-virtual-machine/clone-template-linux-nested-folder/main.tf
+++ vsphere/vsphere-virtual-machine/clone-template-linux-nested-folder/main.tf
@@ -52,9 +52,10 @@ resource "vsphere_virtual_machine" "vm" {
     network_id = data.vsphere_network.network.id
   }
   disk {
-    label = "disk0"
-    size  = var.vm_disk_size
-    thin_provisioned = true
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
   }
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
